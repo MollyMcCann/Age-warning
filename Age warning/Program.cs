bd551be5f43ca6a9485762a2e0513eb852ccc1fa
Molly Mccann <mollymccann@bellevuecollege.edu>
2020-08-25 08:29:02 -0700

    Add project files.

---
+++ Age warning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Age_warning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age: ");
+            byte age = Byte.Parse(Console.ReadLine());
+
+            if (age < 17)
+            {
+                Console.WriteLine("Recommended age is 17!");
+               
+            }
+            Console.WriteLine("Enjoy your game");
+            Console.ReadKey();
+            
+        }
+    }
+}
